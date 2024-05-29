1cd1d012971b11c1bb327fd8e00e1a0d76eca0ac
Sakha_Naufal_Huda <sakhanaufal2@gmail.com>
2024-05-29 23:45:06 +0700

    bug on header

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { HashRouter, Route, Routes } from 'react-router-dom'
 import HomePage from './pages/homePage'
+import ShopPage from './pages/shopPage'
 import ConstructionPage from './pages/constructionPage'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HashRouter>
       <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/shop' element={<ShopPage/>} />
         <Route path='/' element={<HomePage/>}/>
+        <Route path='/constructions' element={<ConstructionPage/>}/>
       </Routes>
     </HashRouter>
   </React.StrictMode>,
