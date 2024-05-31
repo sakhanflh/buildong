d3652597f214b6b92d420038d1a3adeb15b00a06
Sakha_Naufal_Huda <sakhanaufal2@gmail.com>
2024-05-31 10:43:41 +0700

    delete username in register

--- src/components/layouts/register/RegisterLayout.jsx
+++ src/components/layouts/register/RegisterLayout.jsx
@@ -21,21 +21,6 @@ export function RegisterLayout() {
                     <form className="space-y-6">
                         <div className="relative">
                             <input
-                                type="text"
-                                id="username"
-                                className="block w-full px-4 py-2 text-gray-900 bg-gray-50 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent peer"
-                                placeholder=" "
-                                required
-                            />
-                            <label
-                                htmlFor="username"
-                                className="absolute text-gray-600 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-blue-400 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 left-2"
-                            >
-                                Username
-                            </label>
-                        </div>
-                        <div className="relative">
-                            <input
                                 type="email"
                                 id="email"
                                 className="block w-full px-4 py-2 text-gray-900 bg-gray-50 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent peer"
