3ab99cbc01958989e3ab50d9365577e8a704fc60
Berlandro <bryangodlive0@gmail.com>
2024-06-26 00:50:25 +0700

    UI : Update UI at construction page

---
+++ src/components/fragments/ReviewCard.jsx
@@ -0,0 +1,21 @@
+import Stars from "../elements/Stars"
+
+const ReviewCard = ({name, img, postAt, star, desc}) => {
+    return (
+        <div className="border-b-2 py-3">
+            <div className="flex justify-between items-start">
+                <div>
+                <div className="flex gap-2 mb-2">
+                    <div className="w-7 h-7 rounded-full bg-neutral-300"></div>
+                    <h1 className="font-semibold">{name}</h1>
+                </div>
+                <Stars length={star}/>
+                </div>
+                <p className="text-neutral-400">{postAt}</p>
+            </div>
+            <p className="text-neutral-500 mt-3">{desc}</p>
+        </div>
+    )
+}
+
+export default ReviewCard;
