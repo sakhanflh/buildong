520d4f28b661d52695e351799d3341e90e00f908
Berlandro <bryangodlive0@gmail.com>
2024-06-29 15:28:56 +0700

    FEAT : add some feature to completed order page

--- src/components/layouts/order/AddressLayout.jsx
+++ src/components/layouts/order/AddressLayout.jsx
@@ -4,14 +4,14 @@ import Loader from "../../fragments/Loader"
 const AddressLayout = ({account}) => {
     return (
         <div className=" rounded-lg bg-white shadow-soft px-6 py-6">
-                            <h1 className="font-bold xl:text-lg">Shipping Address</h1>
+            <h1 className="font-bold xl:text-lg">Shipping Address</h1>
             {
             !account ?
-            <div className="mt-6 flex justify-center">
+            <div className="mt-4 xl:mt-6 flex justify-center">
                 <Loader color={'bg-primary'}/>
             </div>
             :
-            <div className="mt-6 border-blue-200 bg-blue-50 px-4 py-4 border-2 rounded-lg max-w-80">
+            <div className="mt-4 xl:mt-6 border-blue-200 text-sm xl:text-base bg-blue-50 px-4 py-4 border-2 rounded-lg max-w-80">
                 <h1>{account.phone}</h1>
                 <h2 className="font-semibold">{account.address}</h2>
             </div>
