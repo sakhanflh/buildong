01ed582faed777d1fa83ebba6bb8bf60998beda3
Sakha_Naufal_Huda <sakhanaufal2@gmail.com>
2024-07-01 00:50:14 +0700

    Master: manage conflict on main

--- src/main.jsx
+++ src/main.jsx
@@ -12,12 +12,9 @@ import ProfilePage from './pages/profilePage'
 import ShopPage from './pages/shopPage'
 import AdminPage from './pages/adminPage'
 import OrderSumPage from './pages/orderSumPage'
-<<<<<<< HEAD
 import ProjectPage from './pages/projectPage'
 import CartPage from './pages/cartPage'
-=======
 import OrderDetailLayout from './components/layouts/profile/OrderDetailLayout'
->>>>>>> bfdb0db4058242726cb995df0f22099a8cc64918
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -29,11 +26,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           <Route path='/register' element={<RegisterPage />} />
           <Route path='/profile/:profileId' element={<ProfilePage/>} />
           <Route path='/profile/:profileId/:statusId' element={<ProfilePage/>} />
-<<<<<<< HEAD
           <Route path='/project' element={<ProjectPage/>} />
-=======
           <Route path='/:orderId' element={<OrderDetailLayout/>} />
->>>>>>> bfdb0db4058242726cb995df0f22099a8cc64918
           <Route path='/shop' element={<ShopPage />} />
           <Route path='/constructions' element={<ConstructionPage/>}/>
           <Route path='/constructions/:constructionId' element={<ConstructionDetailsPage/>}/>
